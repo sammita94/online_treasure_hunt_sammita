91c7ebd6a092512259b40e9c1882f93612db26fe
sammita94 <sammitachakravarti@yahoo.com>
2015-12-17 14:25:07 +0530

    changes

--- oth/views.py
+++ oth/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth import models
+from django.contrib import messages
 from oth import models
 m_level = 1
 f_user = ""
@@ -75,27 +76,31 @@ def answer(request):
         answer = request.POST.get('ans')
     player = models.player.objects.get(user_id=request.user.pk)
     try:
-        level = models.level.objects.get(l_number=player.max_level)	
-        print answer
-        print level.answer
-        if answer == level.answer:
-	   	print level.answer
-		player.max_level = player.max_level + 1
-	#print player.max_level
-		global m_level
-		global f_user        
-		if m_level<player.max_level:
-	    	    m_level=player.max_level
-	            f_user = player.name	
-	   	    player.save()
-	try:	
-		level = models.level.objects.get(l_number=player.max_level)
-		return render(request, 'level.html' , {'player': player , 'level': level})
-	except:
-		return render(request, 'finish.html' , {'player': player})
-    	return render(request, 'level.html' , {'player': player , 'level': level})
+        level = models.level.objects.get(l_number=player.max_level)
     except:
 	return render(request, 'finish.html' , {'player': player})
+    right = 0	
+    print answer
+    print level.answer
+    if answer == level.answer:
+	print level.answer
+	right = 1;		
+	player.max_level = player.max_level + 1
+	#print player.max_level
+	global m_level
+	global f_user        
+	if m_level<player.max_level:
+	    m_level=player.max_level
+	    f_user = player.name	
+	    player.save()
+	try:	
+	    level = models.level.objects.get(l_number=player.max_level)
+	    return render(request, 'level.html' , {'player': player , 'level': level})
+	except:
+            return render(request, 'finish.html' , {'player': player})
+    messages.error(request, "Wrong Answer!, Try Again")    
+    return render(request, 'level.html' , {'player': player , 'level': level})
+    
 
 
        
